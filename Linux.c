1801d287bb1dc4f96fa6e99eeb33c96d267374b0
EnmanuelOchoaPerez <149899527+EnmanuelOchoaPerez@users.noreply.github.com>
2026-06-01 15:23:13 +0000

    un while de prueba y los nombres de los participantes

--- Linux.c
+++ Linux.c
@@ -1,2 +1,65 @@
 // https://potential-fishstick-7vvvjq49vqx7cr75w.github.dev/(ide)
 #include <stdio.h>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+while ((i = 0) == 5){
+    printf("[1] Crear usuario \n");
+    printf(" [2] Listar todos los usuarios \n");
+    printf(" [4] Bloquear / desbloquear usuario \n");
+    printf(" [5] Eliminar usuario \n");
+    i++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Ochoa Pérez Enmanuel Ignacio 
+//Negrete Alvarado Daniel José 
