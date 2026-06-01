cad58552d79e9ed7aafa04c1f159f179eff84460
EnmanuelOchoaPerez <149899527+EnmanuelOchoaPerez@users.noreply.github.com>
2026-06-01 10:53:27 -0500

    Refactor user creation logic with do-while loop

--- Linux.c
+++ Linux.c
@@ -10,45 +10,47 @@ struct Usuario{
     signed estado;
 }
 
-if (i== 0){
-
-    FILE *archivo = fopen("usuarios.dat", "w");
-    if (archivo == NULL) {
-        printf("Error al crear el archivo.\n");
-        return 1;
-    }
-
-    fprintf("Ingrese nombre:  %s\n ", Usuario.nombre);
-
-    fprintf("Ingrese apellido: %s\n ", Usuario.apellido);
-
-    Usuario.usuario[0]= Usuario.nombre[0]
-
-    for[int i=0 ; i<5 ; i++]{
-        Usuario.usuario[i +1]= Usuario.apellido[i];
-    }
-    Usuario.usuario[6] = '\0';
-    
-    fprintf("Su usuario es: %s\n", Usuario.usuario);
-
-    fprintf("Que grupo pertenece: %s\n ", Usuario.grupo);
-
-    fprintf("Contraseña: %d\n ", Usuario.clave);
-
-    signed Usuario.estado= 1;
-    fprintf(archivo, "Estado ACTIVO %d\n", Usuario.estado);
-
-    fclose(archivo);
-    printf("Datos de texto guardados con exito.\n");
-    return 0;
-}
-
-while ((i = getchar()) != '0'){
+do {
     printf(" [1] Crear usuario \n");
+    
+    if (i == 1){
+
+        FILE *archivo = fopen("usuarios.dat", "w");
+        if (archivo == NULL) {
+            printf("Error al crear el archivo.\n");
+        return 1;
+        }
+
+        fprintf("Ingrese nombre:  %s\n ", Usuario.nombre);
+    
+        fprintf("Ingrese apellido: %s\n ", Usuario.apellido);
+    
+        Usuario.usuario[0]= Usuario.nombre[0]
+    
+        for[int i=0 ; i<5 ; i++]{
+            Usuario.usuario[i +1]= Usuario.apellido[i];
+        }
+        Usuario.usuario[6] = '\0';
+        
+        fprintf("Su usuario es: %s\n", Usuario.usuario);
+    
+        fprintf("Que grupo pertenece: %s\n ", Usuario.grupo);
+    
+        fprintf("Contraseña: %d\n ", Usuario.clave);
+    
+        signed Usuario.estado= 1;
+        fprintf(archivo, "Estado ACTIVO %d\n", Usuario.estado);
+    
+        fclose(archivo);
+        printf("Datos de texto guardados con exito.\n");
+        return 0;
+    }
+
     printf(" [2] Listar todos los usuarios \n");
     printf(" [4] Bloquear / desbloquear usuario \n");
     printf(" [0] Eliminar usuario \n");
-}
+    if
+}while ((i = getchar()) != '0')
 
 
 
